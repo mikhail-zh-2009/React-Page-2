83a47829513078dcd67b2bef1ead93b7086c7678
Mihail Zhizhikin <mihailzizikin330@gmail.com>
2022-08-05 15:03:58 +0500

    Begin.

---
+++ App.js
@@ -0,0 +1,19 @@
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+
+// Button main class
+class Button extends React.Component {
+    constructor(props) {
+        super(props)
+        this.handleClick = this.handleClick.bind(this)
+    }
+}
+
+function App() {
+    //Buttons that will be on page
+    return <div>
+        
+    </div>
+}
+
+root.render(<App/>)
