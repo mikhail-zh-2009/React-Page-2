d1bda9c21889efe06c31204ba9774a213d6a4fb5
Mihail Zhizhikin <mihailzizikin330@gmail.com>
2022-08-05 16:14:07 +0500

    Added 'Button' class in App.js and styles for it in style.css

--- App.js
+++ App.js
@@ -5,11 +5,21 @@ class Button extends React.Component {
         super(props)
         this.handleClick = this.handleClick.bind(this)
     }
+    handleClick() {
+        this.props.event()
+    }
+    render() {
+        return <button id={this.props.ID} className={this.props.Class} onClick={this.handleClick}>{this.props.text}</button>
+    }
+}
+
+function testing() {
+    console.log('abc');
 }
 
 function App() {
     return <div>
-        
+        <Button ID="button_1" Class="Button" text="Hello World!" event={testing}/>
     </div>
 }
 
